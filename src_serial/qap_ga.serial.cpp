1326c5cc20e2dbafcaba2af0024faf146d740cd5
Mohamed Amine Abdellaziz <abdellazizamine@posteo.net>
2021-02-27 23:53:20 +0100

    imade the parallel and serial do the same things

--- src_serial/qap_ga.serial.cpp
+++ src_serial/qap_ga.serial.cpp
@@ -12,9 +12,9 @@
 #include "individual.serial.hpp"
 #include "matrix.serial.hpp"
 
-#define pop_size 100
-#define nb_gen 20
-#define no_improvenment_max 5
+#define pop_size 1000
+#define nb_gen 250
+#define no_improvenment_max 25
 
 
 // Function to open a data file of the qaplib 
@@ -60,7 +60,7 @@ int open_file_soln(std::ifstream &file_soln, double &Value, std::vector<int> &So
 	file_soln >> Value;
 	// We empty the vector
 	Solution.clear();
-	Solution.reserve(N);
+	Solution.resize(N);
 	for(int i = 0; i < N; i++){
 		int buffer;
 		file_soln >> buffer;
@@ -90,8 +90,9 @@ int main(int argc, char* argv[])
 			std::vector<double> D; // Distance matrix
 			int N = open_file_dat(file_dat, F, D);
 
-			std::vector<Individual *> Population; // The population 
-			Population.reserve(pop_size);
+			// https://stackoverflow.com/questions/19657728/create-a-vector-of-instances-of-a-class-in-c
+			std::vector<Individual> Population(pop_size, Individual(N, r)); // The population 
+			std::vector<Individual> Parents(pop_size, Individual(N, r)); // The population 
 
 			Individual Best(N, r); // The Best solution
 			// WARNING: Evaluation isn't included in other functions, so each time
@@ -103,9 +104,11 @@ int main(int argc, char* argv[])
 			Best.print_permutation();
 			std::cout << "Fitness: " << Best.fitness << std::endl;
 			// We initialize the population
-			for(int i = 0; i < pop_size; i++){
-				Population.push_back(new Individual(N, r));
-				Population[i]->evaluate_trace(F, D);
+			for(int i = 0; i < Population.size(); i++){
+				Individual buffer(N, r);
+				Population[i] = buffer;
+				Population[i].evaluate_trace(F, D);
+				Parents[i] = Population[i];
 			}
 
 			int generation = 0; // Number of generations
@@ -122,41 +125,52 @@ int main(int argc, char* argv[])
 				std::cout<<"==============================\n";
 				std::cout<<"Generation: "<<generation<<"\n";
 
+				// We make a copy of the population; it will represent the parents and we need it so we do not change 
+				// the individuals/parents during the process
+				for(int i = 0; i < Population.size(); i++){
+					Parents[i] = Population[i];
+				}
+
 				for(int i = 0; i < Population.size(); i++){
 					std::mt19937 g(r());
 					double operator_probability;
 					operator_probability = U_distr(g); // Uniformly select a random real in [0, 1]
 
-					// 1. Crossover with a 60% probability
-					if(operator_probability < 0.6){
-						// We randomly select an individual from the population so it becomes the second Parent
-						std::uniform_int_distribution<> distrib(0, Population.size() - 2); 
-						int ind = distrib(g);
-						if(i <= ind) ind++; // We ensure we do not pick the first Parent
-						Individual Offspring = Population[i]->crossover(*(Population[ind]), r);
-						Offspring.evaluate_trace(F, D);
-						// We keep the best individual
-						if( Offspring.fitness <= Population[i]->fitness)
-							*Population[i] = Offspring;
-					}
+					// 1. Crossover 
+					// We randomly select an individual from the previous population so it becomes the second Parent; the first parent will have the same index
+					std::uniform_int_distribution<> distrib(0, Population.size() - 2); 
+					int ind = distrib(g);
+					if(i <= ind) ind++; // We ensure we do not pick the first Parent
+					Individual Offspring = Parents[i].crossover(Parents[ind], r);
 
 					operator_probability = U_distr(g); // Uniformly select a random real in [0, 1]
-					// 2. Mutation with a 60% probability
-					if(operator_probability < 0.1)
-						Population[i]->mutate(r);
+					// 2. Mutation with a 50% probability
+					if(operator_probability < 0.5)
+						Offspring.mutate(r);
+					Offspring.evaluate_trace(F, D);
 
-					// 3. Transposition? Check that, wasn't implemented
-					Population[i]->evaluate_trace(F, D);
-					// The infamous 2-opt heuristic to be applied on our individual
-					Population[i]->heuristic_2opt(F, D);
-					//std::cout<<"Value of the Loss function: " << Population[i]->fitness<<"\n";
+					// We keep the best individual between the offspring and its parent with a probability of 90%
+					operator_probability = U_distr(g); // Uniformly select a random real in [0, 1]
+					if(operator_probability < 0.9){
+						if(Offspring.fitness < Parents[i].fitness){
+							Population[i] = Offspring;
+							Population[i].evaluate_trace(F, D);
+						}
+						// no need to copy an individual in population from parents because the former is already a copy of the later
+					}
+					else{
+						if(Offspring.fitness >= Parents[i].fitness){
+							Population[i] = Offspring;
+							Population[i].evaluate_trace(F, D);
+						}
+					}
 
 					// We update the Best solution if we find a better individual
-					if(Population[i]->fitness < Best.fitness){
+					if(Population[i].fitness < Best.fitness){
 						std::cout<<"---------------- A new Best found\n";
 						std::cout<<"Before: "<< Best.fitness;
-						Best = *Population[i];
-						std::cout << " and after: " << Population[i]->fitness << "\n";
+						Best = Population[i];
+						std::cout << " and after: " << Population[i].fitness << "\n";
 						best_generation = generation;
 						// We reset the no_improvement iterator
 						no_improvement = 0;
@@ -172,7 +186,7 @@ int main(int argc, char* argv[])
 			std::cout<<"Best solution found:\n";
 			Best.print_permutation();
 			std::cout<<"Fitness: " << Best.fitness << "\nExecution time: " << time << " s\nGeneration: " << best_generation << "\n";
-			
+
 			if(argc >= 3){
 				std::ifstream file_soln;
 				file_soln.open(argv[2]);
@@ -180,27 +194,27 @@ int main(int argc, char* argv[])
 					std::vector<int> optimal_permutation;
 					double Optimal_Value;
 					open_file_soln(file_soln, Optimal_Value, optimal_permutation);
-						std::cout<<"======================================= Known optimal solution ============================\n";
-						Individual Optimal(optimal_permutation);
-						Optimal.evaluate_trace(F, D);
-						std::cout<<"Known optimal solution:\n";
-						Best.print_permutation();
-						std::cout << "Optimal value (in the file): " << Optimal_Value << "\n";
-						std::cout << "Optimal value (computed): " << Optimal.fitness << "\n";
-					}
+					std::cout<<"======================================= Known optimal solution ============================\n";
+					Individual Optimal(optimal_permutation); // was used to test the evaluate_fitness function; the print_permutation is handy
+					Optimal.evaluate_trace(F, D);
+					std::cout<<"Known optimal solution:\n";
+					Optimal.print_permutation();
+					std::cout << "Optimal value (in the file): " << Optimal_Value << "\n";
+					std::cout << "Best solution found by the genetic algorithm: " << Best.fitness << "\n";
 				}
+			}
 
-			}
-			else{
-				std::cout<<"Problem while opening the data file " << argv[1] << ".\n";
-				return 2;
-			}
 		}
 		else{
-			std::cout<<"Please specify a data file to open. Usage:\n";
-			std::cout<<"./qap_ga.out file.dat\n";
-			return 1;
+			std::cout<<"Problem while opening the data file " << argv[1] << ".\n";
+			return 2;
 		}
+	}
+	else{
+		std::cout<<"Please specify a data file to open. Usage:\n";
+		std::cout<<"./qap_ga.out file.dat\n";
+		return 1;
+	}
 
-        return 0;
+	return 0;
 }
